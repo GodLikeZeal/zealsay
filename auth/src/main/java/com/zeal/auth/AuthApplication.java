cfc5a6af06a9d52831d2966668e39afcc967dda8
zeal <zhangleifor@163.com>
2018-04-10 00:38:56 +0800

    新增
    Signed-off-by: zeal <zhangleifor@163.com>

--- auth/src/main/java/com/zeal/auth/AuthApplication.java
+++ auth/src/main/java/com/zeal/auth/AuthApplication.java
@@ -2,7 +2,21 @@ package com.zeal.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+*@description 统一安全认证服务入口
+ *                     获取用户信息也是通过这个应用实现
+ *                      这里既是认证服务器，也是资源服务器
+ *                      EnableResourceServer
+*@author zeal
+*@date 2018-04-05 16:43
+*@version 1.0.0
+*/
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
 
